e02af104b7c60eeb882e9ca0bee1268a76246501
Jim Posen <jim.posen@gmail.com>
2013-08-27 15:47:22 -0400

    Site updated at 2013-08-27 19:47:22 UTC

---
+++ slides/js/slides.js
@@ -0,0 +1,5 @@
+require(['order!../slide_config', 'order!jquery-1.9.0.min', 'order!modernizr.custom.45394',
+         'order!prettify/prettify', 'order!hammer', 'order!slide-controller',
+         'order!slide-deck'], function(someModule) {
+
+});
